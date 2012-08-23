41d87476e5447e35ca2ef2499d8472547704ca65
Daniel Campo <accounts@epiksolcreative.com>
2012-08-23 15:59:13 -0400

    Added conditionals for Phone Number Pattern Check Function

--- campo_daniel_assignment4.js
+++ campo_daniel_assignment4.js
@@ -11,29 +11,41 @@ var checkPhoneNum = function(myString) {
 
 	var stringLength = myString.length; /* Get length of string */
 	var stringMinLength = 10; /* Maximum Length of a Phone Number (Includes Hyphens) */
-	var stringMaxLength = 12; /* Maximum Length of a Phone Number (Includes Hyphens) */	
+	var stringMaxLength = 12; /* Maximum Length of a Phone Number (Includes Hyphens) */
+	var stringSeparator = "-";
 	var firstHyphen = 3, secondHyphen = 7;
 	
 	var firstHyphenCheck = myString.charAt(firstHyphen);
 	var secondHyphenCheck = myString.charAt(secondHyphen);	
-	console.log('String Analysis Results');
-	console.log('-----------------------------------------');	
-	console.log('-----------------------------------------');
-	console.log('First Location: '+firstHyphenCheck);
-	console.log(' ');
-	console.log('Second Location: '+secondHyphenCheck);	
-	console.log(' ');
-	console.log('String Length: '+stringLength);	
-	console.log('-----------------------------------------');
+	console.log("String Analysis Results");
+	console.log("-----------------------------------------");	
+	console.log("-----------------------------------------");
+	console.log("First Location: "+firstHyphenCheck);
+	console.log(" ");
+	console.log("Second Location: "+secondHyphenCheck);	
+	console.log(" ");
+	console.log("String Length: "+stringLength);	
+	console.log("-----------------------------------------");
+	console.log(" ");	
+	console.log("Result");		
+	console.log("-------------");
+	console.log(" ");	
 	
-	if (myString > stringMaxLength) {
-		console.log('Sorry but the value you entered is too long');
+	
+	if (stringLength > stringMaxLength) {
+		console.log("Too Many Characters.");
 		return false;
 	}
 	
-	if (myString < stringMinLength) {
-		console.log('Sorry but the value you entered is too short');
+	else if (stringLength < stringMinLength) {
+		console.log("Too Little Characters.");
 		return false;
+	}
+	
+	else if (firstHyphen != stringSeparator || secondHyphen != stringSeparator) {
+		console.log("Improperly Formatted. Use XXX"+stringSeparator+"XXX"+stringSeparator+"XXXX (Include '"+stringSeparator+"')");
+		return false;
+	
 	}
 	
 	
