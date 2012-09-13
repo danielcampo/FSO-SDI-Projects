4efb69a7a68e3531500b0d6dc284afb6faf0bd82
Daniel Campo <accounts@danielcampo.com>
2012-09-13 02:33:20 -0400

    Defined global variables.

--- campo_daniel_assignment3.js
+++ campo_daniel_assignment3.js
@@ -1,2 +1,6 @@
-
+var mercenaryName = "Daniel",
+	classesMastered = ["Scout", "Assault"],
+	classesMasteredNum = classesMastered.length,
+	dropsuitClasses = ["Logistics", "Scout", "Assault", "Heavy"],
+	masterAllClasses = false;
 
