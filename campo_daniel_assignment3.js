fe62da1ed65df69b29731ad026d1797af8e6a0c9
Daniel Campo <accounts@danielcampo.com>
2012-09-13 02:35:48 -0400

    Added final functions for flowchart.

--- campo_daniel_assignment3.js
+++ campo_daniel_assignment3.js
@@ -2,7 +2,6 @@ var mercenaryName = "Daniel",
 	classesMastered = ["Scout", "Assault"],
 	classesMasteredNum = classesMastered.length,
 	dropsuitClasses = ["Logistics", "Scout", "Assault", "Heavy"],
-
 	masterAllClasses = false;
 
 // A New Day Begins.
@@ -14,6 +13,9 @@ var mercenaryName = "Daniel",
 // Don't need to train.
 if (classesMasteredNum === dropsuitClasses.length) {
 	console.log("As one of the deadliest mercernaries in the Galaxy, I'm proficient in all " + dropsuitClasses.length + " dropsuit classes.");
+
+	// Describe Dislikes
+	classesDisliked();
 }
 
 // Need to train.
@@ -37,4 +39,25 @@ function classesToMaster (classesMasteredNum) {
 	};
 };
 
+// Create List of Dislike Classes
+function classesDisliked() {
+	var dislike1 = "Logistics",
+		dislike2 = "Heavy";
+		dislikeList = [];
+
+	dislikeList.push(dislike1);
+	dislikeList.push(dislike2);
+
+	listDislikeList(dislikeList);
+};
+
+// List Disliked Classes
+function listDislikeList(dislikeList) {
+	for(classesLeft = 0; classesLeft <= dislikeList.length-1; classesLeft++) {
+			console.log("I disliked the " + dislikeList[classesLeft] + " class.");
+	};
+
+};
+
+console.log("Fin.");
 
