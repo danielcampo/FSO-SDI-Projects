49023e63db69856b738dc1c4d49b7592074ca302
Daniel Campo <accounts@danielcampo.com>
2012-09-13 02:33:52 -0400

    Created basic procedure method.

--- campo_daniel_assignment3.js
+++ campo_daniel_assignment3.js
@@ -4,3 +4,19 @@ var mercenaryName = "Daniel",
 	dropsuitClasses = ["Logistics", "Scout", "Assault", "Heavy"],
 	masterAllClasses = false;
 
+// A New Day Begins.
+
+
+// Morning Thoughts
+// Do I need to train?
+
+// Don't need to train.
+if (classesMasteredNum === dropsuitClasses.length) {
+	console.log("As one of the deadliest mercernaries in the Galaxy, I'm proficient in all " + dropsuitClasses.length + " dropsuit classes.");
+}
+
+// Need to train.
+else {
+	console.log("I still have some classes to master until I've mastered all " + dropsuitClasses.length + " dropsuit classes.");
+};
+
