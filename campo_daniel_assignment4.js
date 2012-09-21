594980627ded06b97857b34095e2556d814d3377
Daniel Campo <accounts@danielcampo.com>
2012-09-20 23:28:11 -0400

    Removed all unneccessary comments and  Library is Complete

--- campo_daniel_assignment4.js
+++ campo_daniel_assignment4.js
@@ -3,104 +3,64 @@ Daniel Campo
 Scalable Data Infrastructures - 1209
 Project 4 Assignment
 */
-var console;
 
 /****************************************************/
 /* String Functions */
 /****************************************************/
 /****************************************************/
 
-function stringPhoneCheck() {
+function stringPhoneCheck(myString) {
 	/* Function 1 - Check Phone Number Pattern */
 	/* Does a string follow a 123-456-7890 pattern like a phone number? */
-
-	var checkPhoneNum = function(myString) {
-		var stringLength = myString.length; /* Get length of string */
-
-		var stringMinLength = 10; /* Maximum Length of a Phone Number (Includes Hyphens) */
-		var stringMaxLength = 12; /* Maximum Length of a Phone Number (Includes Hyphens) */
-
-		var stringSeparator = "-";
-
-		var firstHyphenIndex = 3, secondHyphenIndex = 7;
-
-		var firstHyphenChar = myString.charAt(firstHyphenIndex);
-		var secondHyphenChar = myString.charAt(secondHyphenIndex);
-
-		var phoneAreaCode = myString.substring(0, myString.indexOf(stringSeparator, 0));
-		var phonePrefix = myString.substring(myString.indexOf(stringSeparator, 0)+1, myString.lastIndexOf(stringSeparator, 12));
-		var phoneSuffix = myString.substring(8, 12);
+	var stringLength = myString.length, /* Get length of string */
+		stringMinLength = 10, /* Maximum Length of a Phone Number (Includes Hyphens) */
+		stringMaxLength = 12, /* Maximum Length of a Phone Number (Includes Hyphens) */
+		stringSeparator = "-",
+		firstHyphenIndex = 3,
+		secondHyphenIndex = 7,
+		firstHyphenChar = myString.charAt(firstHyphenIndex),
+		secondHyphenChar = myString.charAt(secondHyphenIndex),
+		phoneAreaCode = myString.substring(0, myString.indexOf(stringSeparator, 0)),
+		phonePrefix = myString.substring(myString.indexOf(stringSeparator, 0)+1, myString.lastIndexOf(stringSeparator, 12)),
+		phoneSuffix = myString.substring(8, 12);
 
 
-		if (stringLength > stringMaxLength || stringLength < stringMinLength || firstHyphenChar != stringSeparator || secondHyphenChar != stringSeparator) {
-			return false;
-		}
-
-		else {
-			return true;
-		}; // MAKEUP: missing semi-colons for if/else statements
-
-	};
-
-	var phoneCorrect = "123-456-7890", phoneIncorrect = "1234567890"; // MAKEUP: use double quotes instead of single
-
-	console.log("1: Phone Check Function");
-	console.log("-------------------------");
-
-	/* True */
-	if (checkPhoneNum(phoneCorrect)) {
-		console.log("String: "+phoneCorrect);
-		console.log("Does a string follow a 123-456-7890 pattern like a phone number? "+checkPhoneNum(phoneCorrect));
+	if (stringLength > stringMaxLength || stringLength < stringMinLength || firstHyphenChar != stringSeparator || secondHyphenChar != stringSeparator) {
+		return false;
 	}
 
-	console.log(" ");
-
-	/* False */
-	if (!checkPhoneNum(phoneIncorrect)) {
-		console.log("String: "+phoneIncorrect);
-		console.log("Does a string follow a 123-456-7890 pattern like a phone number? "+checkPhoneNum(phoneIncorrect));
+	else {
+		return true;
 	};
-
-	console.log("-------------------------");
-	console.log("-------------------------");
-	/* Function 1 - Check Phone Number Pattern */
-	/****************************************************/
 };
 
-stringPhoneCheck(); console.log(" ");
-
-
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 
-
-
-function stringEmailCheck() {
+function stringEmailCheck(myString) {
 	/* Function 2 - Check Email Pattern */
 	/* Does a string follow an aaa@bbb.ccc pattern like an email address? */
-	var checkStringEmail = function(myString) {
-
-		var emailStringLength = myString.length;
+	var emailStringLength = myString.length;
 
 
-		/* Illegal Characters */
-		/* Check String for Illegal Characters */
-		var emailIllegalSymbols = [
-			"!", "#", "$" , "%" ,"&" , "'", "\"" ,"*" , "+" , "," , "=" , "?" , "^" , "`" , "{" , "|" , "}" ,"~", " "
-		];
+	/* Illegal Characters */
+	/* Check String for Illegal Characters */
+	var emailIllegalSymbols = [
+		"!", "#", "$" , "%" ,"&" , "'", "\"" ,"*" , "+" , "," , "=" , "?" , "^" , "`" , "{" , "|" , "}" ,"~", " "
+	];
 
 
-		for (symbol in emailIllegalSymbols) {
-			var emailIllegalSymbolIndexes = myString.indexOf(emailIllegalSymbols[symbol], 0); /* Location of Illegal Symbols in myString */
+	for (symbol in emailIllegalSymbols) {
+		var emailIllegalSymbolIndexes = myString.indexOf(emailIllegalSymbols[symbol], 0); /* Location of Illegal Symbols in myString */
 
-			/* Return False if String Contains Illegal Characters */
-			if (emailIllegalSymbolIndexes != -1) {
-				return false;
-			};
+		/* Return False if String Contains Illegal Characters */
+		if (emailIllegalSymbolIndexes != -1) {
+			return false;
 		};
-		/* END Illegal Characters */
+	};
+	/* END Illegal Characters */
 
 
 		/* @ Symbol */
@@ -115,133 +75,64 @@ function stringEmailCheck() {
 		/* END @ Symbol */
 
 
-		/* Email Domain Name */
-		/* Check String for a Proper Domain Suffix */
-		var emailDomainSeparator = '.';
-		var emailDomainSeparatorIndex = myString.indexOf(emailDomainSeparator, 0); /* Location of . Symbol in myString */
+			/* Email Domain Name */
+			/* Check String for a Proper Domain Suffix */
+			var emailDomainSeparator = '.';
+			var emailDomainSeparatorIndex = myString.indexOf(emailDomainSeparator, 0); /* Location of . Symbol in myString */
 
-		var emailDomain = myString.substring(emailDomainSeparatorIndex, emailStringLength);
-		var emailDomainLength = emailDomain.length;
-		var emailDomainMaxChar = 4, emailDomainMinChar = 2; /* Min and Max Sizes for Domain Names */
+			var emailDomain = myString.substring(emailDomainSeparatorIndex, emailStringLength);
+			var emailDomainLength = emailDomain.length;
+			var emailDomainMaxChar = 4, emailDomainMinChar = 2; /* Min and Max Sizes for Domain Names */
 
-		/* Fail if there is there are issues with the domain name */
-		if (emailDomainSeparatorIndex == -1 || emailDomainLength < emailDomainMinChar || emailDomainLength > emailDomainMaxChar) {
-			return false;
-		};
-		/* END Email Domain Name */
-
-		var emailPrefix = myString.substring(0, emailAtSymbolIndex);
-		var emailSuffix = myString.substring(emailAtSymbolIndex+1, emailDomainSeparatorIndex);
-
-		return true;
+			/* Fail if there is there are issues with the domain name */
+			if (emailDomainSeparatorIndex == -1 || emailDomainLength < emailDomainMinChar || emailDomainLength > emailDomainMaxChar) {
+				return false;
+			};
+			/* END Email Domain Name */
 
 
-	};
 
-	var emailCorrect = "abc@123.com", emailIncorrect = "abc123.com";
+	var emailPrefix = myString.substring(0, emailAtSymbolIndex);
+	var emailSuffix = myString.substring(emailAtSymbolIndex+1, emailDomainSeparatorIndex);
 
-	console.log("2: Email Check Function");
-	console.log("-------------------------");
-
-	/* True */
-	if (checkStringEmail(emailCorrect)) {
-		console.log("String: "+emailCorrect);
-		console.log("Does a string follow an aaa@bbb.ccc pattern like an email address? "+checkStringEmail(emailCorrect));
-	};
-
-	console.log(" ");
-
-	/* False */
-	if (!checkStringEmail(emailIncorrect)) {
-		console.log("String: "+emailIncorrect);
-		console.log("Does a string follow an aaa@bbb.ccc pattern like an email address? "+checkStringEmail(emailIncorrect));
-	};
-
-	console.log("-------------------------");
-	console.log("-------------------------");
-	/* Function 2 - Check Email Pattern */
-	/****************************************************/
+	return true;
 };
 
-stringEmailCheck(); console.log(" ");
-
-
-
-
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 
-
-function stringCheckUrl() {
+function stringCheckUrl(myString) {
 	/* Function 3 - Check URL */
 	/* Is the string a URL? (Does it start with http: or https:?) */
-	var checkStringURL = function(myString) {
+	var stringHttp = "http://", stringHttps = "https://";
 
-		var stringHttp = "http://", stringHttps = "https://";
+	var httpRegular = myString.substring(0,stringHttp.length);
+	var httpSecure = myString.substring(0,stringHttps.length);
 
-		var httpRegular = myString.substring(0,stringHttp.length);
-		var httpSecure = myString.substring(0,stringHttps.length);
+	/* Return False If String Does Not Begin with Regular HTTP */
+	if (httpRegular == stringHttp ) {
+		return true;
+	}
 
-		/* Return False If String Does Not Begin with Regular HTTP */
-		if (httpRegular == stringHttp ) {
-			return true;
-		}
+	else if (httpSecure == stringHttps ) {
+		return true;
+	}
 
-		else if (httpSecure == stringHttps ) {
-			return true;
-		}
-
-		else {
-			return false;
-		};
+	else {
+		return false;
 	};
-	/* Function 3 - Check URL */
-	/****************************************************/
-
-		var linkCorrect = "http://test.com/", linkIncorrect = "link://test.com/";
-
-		console.log("3: URL Check Function");
-		console.log("-------------------------");
-
-		/* True */
-		if (checkStringURL(linkCorrect)) {
-			console.log("String: "+linkCorrect);
-			console.log("Is the string a URL? (Does it start with http: or https:?)? "+checkStringURL(linkCorrect));
-		}
-
-		console.log(" ");
-
-		/* False */
-		if (!checkStringURL(linkIncorrect)) {
-			console.log("String: "+linkIncorrect);
-			console.log("Is the string a URL? (Does it start with http: or https:?)? "+checkStringURL(linkIncorrect));
-		}
-
-		console.log("-------------------------");
-		console.log("-------------------------");
-		/* Function 3 - Check URL */
-		/****************************************************/
 };
 
-stringCheckUrl(); console.log(" ");
-
-
-
-
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 
-
-
-function stringTitleCase() {
+function stringTitleCase(myString) {
 	/* Function 4 - Case String */
 	/* Title-case a string (split into words, then uppercase the first letter of each word) */
-	var titleCaseString = function(myString) {
-
 		var myStringLength = myString.length;
 		var result = "";
 
@@ -305,67 +196,82 @@ function stringTitleCase() {
 
 			return result;
 		};
-
-	};
-
-
-	var myString = "this is a string with spaces";
-
-	console.log("4: String Title Case Function");
-	console.log("-------------------------");
-
-	console.log("Original String: "+ myString);
-	console.log("Result: "+ titleCaseString(myString));
-
-	console.log("-------------------------");
-	console.log("-------------------------");
-	/* Function 4 - Case String */
 };
 
-stringTitleCase(); console.log(" ");
-
-
-
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 
-
-
-function stringSeparatorChanger() {
+function stringSeparatorChanger(myString, alternateSeparator) {
 	/* Function 5 - Change List Separator */
 	/*
-		Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
+		Given a string that is a list of things separated by a given string, as well as another string separator, return a string
+		with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
 	*/
-	var changeListSeparator = function(myString, alternateSeparator) {
-		if (!alternateSeparator) { alternateSeparator = "/"; };
-		return myString.replace(/,/g, alternateSeparator);
-	};
+	if (!alternateSeparator) { alternateSeparator = "/"; };
+	return myString.replace(/,/g, alternateSeparator);
+};
+
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+
+/****************************************************/
+/* Number Functions */
+/****************************************************/
+/****************************************************/
+
+function numberSetDecimalPoint(myNumber, numberDecimalPoints) {
+	/* Function 6 - Set Amount of Trailing Decimal Points */
+	/* Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10 */
+	return myNumber.toFixed(numberDecimalPoints);
+};
 
 
-	var myList = "a,b,c,d";
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
 
-	console.log("5: Change List Separator Function");
-	console.log("-------------------------");
 
-	console.log("String: "+ myList);
-	console.log("String [Revised]: "+ changeListSeparator(myList, "-"));
+function numberDaysBetweenTwoDates(date1, date2) {
+	/* Function 7 - Number of Days Between Two Dates */
+	/* Find the number of hours or days difference between two dates. */
+		var daySingle = 1000 * 60 * 60 * 24;
 
-	console.log("-------------------------");
-	console.log("-------------------------");
+		var date1 = new Date(date1);
+		var date2 = new Date(date2);
 
-	/* Function 5 - Change List Separator */
-	/****************************************************/
+		date1_ms = date1.getTime();
+		date2_ms = date2.getTime();
+
+		return (date2_ms-date1_ms)/daySingle;
 
 };
 
-stringSeparatorChanger(); console.log(" ");
-
-
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 /*************************************************************************************/
 
+function stringToNumber(myString) {
+	/* Function 8 - String to Number */
+	/* Given a string version of a number such as "42", return the value as an actual Number, such as 42. */
+		if (!isNaN(parseInt(myString))) {
+			return parseInt(myString);
+		}
 
+		else {
+			return "Not a Number.";
+		};
+};
